38ffd720b4addd3442d7057ec87a8eaf5b264f43
joseamundaray <amundaray2020@gmail.com>
2024-05-17 21:46:31 -0300

    Validación del Formulario con JS

--- js/main.js
+++ js/main.js
@@ -26,22 +26,49 @@ if(navClose) {
 // JS Validación de Registro //
 
 const nombre = document.getElementById("name")
-
 const email = document.getElementById("email")
-
 const pass = document.getElementById("password")
-
+const confirmacion = document.getElementById("confirm-pass")
 const form = document.getElementById("form")
-
-const parrafo = document.getElementById("warning")
+const parrafo = document.getElementById("warnings")
+const checkbox = document.getElementById("terminos")
 
 form.addEventListener("submit", e=>{
-e.preventDefault()
-let warning = ""
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    parrafo.innerHTML = ""
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-if(nombre.value.lenght <6){
-warning += `el nombre no valido <br>`}
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
 
-if(!regex.Ealue.lenght <6){
-warning += `el nombre no valido <br>`}
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+
+    if(pass.value.length <8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+
+    if(confirmacion.value !== pass.value){
+        warnings += `Las contraseñas no coinciden <br>`
+        entrar = true
+    }
+
+    if(checkbox.checked !== true){
+        warnings += `Debes aceptar los terminos y condiciones <br>`
+        entrar = true
+    }
+
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Ya estas registrado! <3"
+    }
 })
